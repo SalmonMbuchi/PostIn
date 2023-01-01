70b46bfb51c7d62a80194d395c7acdcfe029f598
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-01-01 15:24:02 +0300

    authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -68,6 +68,9 @@ def get_locale():
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
 
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
 # avoid circular imports
 from app import errors, routes, models
 
